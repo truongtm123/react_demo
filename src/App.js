7501194d1510706254f99e311ce9424db84b7adf
DESKTOP-6J3G1B7\trung <truongtm@egdgroup.com>
2023-06-09 16:31:23 +0700

    fix bug

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import MyButton from './MyButton';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.123
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
@@ -17,7 +18,9 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <h1>Welcome my app</h1>
+      <MyButton />
     </div>
   );
 }
---
+++ src/MyButton.js
@@ -0,0 +1,10 @@
+import logo from './logo.svg';
+import './App.css';
+
+function MyButton() {
+  return (
+    <button>Test</button>
+  );
+}
+
+export default MyButton;
